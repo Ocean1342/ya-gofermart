f5dec29fa9b72c05caafc83e17db48a645f800db
Pashichev Anton <anton.pashichev@lenvendo.ru>
2025-08-19 19:29:40 +0300

    iter2: add loadOrded handler

--- config/config.go
+++ config/config.go
@@ -19,7 +19,7 @@ type Config struct {
 func New() *Config {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Errorf("Error loading .env file")
 	}
 
 	ttl, err := strconv.Atoi(os.Getenv("TOKEN_TTL"))
