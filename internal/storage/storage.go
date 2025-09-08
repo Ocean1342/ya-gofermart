0ef48b2ac13019bbd866fb6b46e92ea6fff81cef
Pashichev Anton <a.pashichev@rambler-co.ru>
2025-09-08 18:45:51 +0300

    iter6: add pool

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,20 +2,30 @@ package storage
 
 import (
 	"context"
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	log "github.com/sirupsen/logrus"
 	"os"
 )
 
 type PGStorage struct {
-	Connection *pgx.Conn
+	Connection *pgxpool.Pool
 }
 
 func New(ctx context.Context, databaseURL string) Storage {
-	conn, err := pgx.Connect(ctx, databaseURL)
+	config, err := pgxpool.ParseConfig(databaseURL)
+	if err != nil {
+		log.Fatalf("could not parse db string: %v", err)
+	}
+
+	config.MaxConns = 20
+	config.MinConns = 5
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	return &PGStorage{Connection: conn}
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &PGStorage{Connection: pool}
 }
