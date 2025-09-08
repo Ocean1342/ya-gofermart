3e3f966f52cd6b2db087d112bb29cd1a2728f24a
Pashichev Anton <a.pashichev@rambler-co.ru>
2025-09-08 19:35:58 +0300

    iter6: add go

--- internal/order-processor/processor.go
+++ internal/order-processor/processor.go
@@ -41,33 +41,35 @@ func (op *OrderProcessor) Process(ctx context.Context) {
 	logger := logrus.WithFields(map[string]interface{}{
 		"HANDLER": "OrderProcessor",
 	})
-	//queueTicker := time.NewTicker(op.tick)
-	//storageInterrogateTicker := time.NewTicker(op.tick + 1)
+	queueTicker := time.NewTicker(op.tick)
+	storageInterrogateTicker := time.NewTicker(op.tick + 1)
 	for {
 		select {
 		case <-ctx.Done():
 			logger.Info("Order processor stop by ctx")
 			return
-		//case <-storageInterrogateTicker.C:
-		//go op.interrogateProcess(ctx)
-		/*		case <-queueTicker.C:
-				item, ok := <-op.Queue
-				if !ok {
-					logger.Info("queue channel closed")
-					return
-				}
-				if item.OrderID != 0 && item.UserID != 0 {
-					logger.Infof("ticker recieved item orderID:%d, userID: %d retrytimes: %d", item.OrderID, item.UserID, item.RetryTimes)
-					op.process(ctx, *logger, item)
-				}*/
+		case <-storageInterrogateTicker.C:
+			go op.interrogateProcess(ctx)
+		case <-queueTicker.C:
+			item, ok := <-op.Queue
+			if !ok {
+				logger.Info("queue channel closed")
+				return
+			}
+			if item.OrderID != 0 && item.UserID != 0 {
+				logger.Infof("ticker recieved item orderID:%d, userID: %d retrytimes: %d", item.OrderID, item.UserID, item.RetryTimes)
+				go op.process(ctx, *logger, item)
+			}
 		case item := <-op.Queue:
 			logger.Infof("recieved item orderID:%d, userID: %d retrytimes: %d", item.OrderID, item.UserID, item.RetryTimes)
-			op.process(ctx, *logger, item)
+			go op.process(ctx, *logger, item)
 		}
 	}
 }
 
 func (op *OrderProcessor) process(ctx context.Context, logger logrus.Entry, item OrderQueueItem) {
+	ctxProcess, cancel := context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
 	resp := op.AccrualService.OrderProcess(item.OrderID)
 	if resp == nil {
 		logger.Errorf("empty response from accrualService")
@@ -100,7 +102,7 @@ func (op *OrderProcessor) process(ctx context.Context, logger logrus.Entry, item
 			return
 		}
 		logger.Infof("user id %d get raw accrual: %f counted accrual: %d", item.UserID, acResp.Accrual, acResp.GetAccrual())
-		err = op.Storage.UpdateOrderAndBalance(ctx, orderID, item.UserID, acResp.GetAccrual(), acResp.GetStatus())
+		err = op.Storage.UpdateOrderAndBalance(ctxProcess, orderID, item.UserID, acResp.GetAccrual(), acResp.GetStatus())
 		if err != nil {
 			logger.Errorf("UpdateOrderAndBalance err: %v", err)
 			item.RetryTimes++
@@ -113,10 +115,10 @@ func (op *OrderProcessor) process(ctx context.Context, logger logrus.Entry, item
 			logger.Errorf("recived 204. orderID %d read body err:%s", item.OrderID, err)
 		}
 		logger.Errorf("recived 204. orderID %d response body: %s", item.OrderID, string(bytes))
-		/*		err = op.Storage.UpdateOrderAndBalance(ctx, item.OrderID, item.UserID, 0, "INVALID")
-				if err != nil {
-					logger.Errorf("could not change status for order id:%d err:%v", item.OrderID, err)
-				}*/
+		err = op.Storage.UpdateOrderAndBalance(ctxProcess, item.OrderID, item.UserID, 0, "INVALID")
+		if err != nil {
+			logger.Errorf("could not change status for order id:%d err:%v", item.OrderID, err)
+		}
 	case 429:
 		logger.Errorf("too many requests.sleeps for 60 sec")
 		time.Sleep(60 * time.Second)
