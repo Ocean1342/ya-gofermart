9cad71345e5cb6de005af438a10c450fe98f330d
Pashichev Anton <a.pashichev@rambler-co.ru>
2025-09-10 20:04:20 +0300

    fix vet

--- internal/order-processor/processor.go
+++ internal/order-processor/processor.go
@@ -72,6 +72,12 @@ func (op *OrderProcessor) process(ctx context.Context, logger logrus.Entry, item
 		op.Queue <- item
 		return
 	}
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			logger.Errorf("could not close response body. err: %v", err)
+		}
+	}()
 	switch resp.StatusCode {
 	case 200:
 		err := op.StatusOkHandle(ctx, logger, resp, item)
